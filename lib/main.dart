d861a0e7f9d53d6bb1bda62b82902df2c5fa8a1c
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-02-03 22:32:42 +0530

    Carousel Slider & Elastic Drawer

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:ui_development/pages/slider.dart';
+
+void main() => runApp(
+    const MaterialApp(debugShowCheckedModeBanner: false, home: Carousel()));

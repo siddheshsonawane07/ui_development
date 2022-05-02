2ba71849ecc764a5a176c92ef792c5c8bc5f2b06
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-05-02 23:15:00 +0530

    concentric

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ui_development/pages/concentric.dart';
 
 void main() => runApp(
-    const MaterialApp(debugShowCheckedModeBanner: false, home: ));
+    const MaterialApp(debugShowCheckedModeBanner: false, home:ConcentricTransition()));
